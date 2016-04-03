f3c4bd0e7ea13779a77f4f623799d4692fcd2e1f
zhengwenbing <zhengwenbing@meituan.com>
2016-04-03 13:21:24 +0800

    fix multiple assets

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var _ = require('lodash');
 var fs = require('fs');
+var path = require('path');
+
+var forOwn = require('lodash/forOwn');
 
 var ManifestGeneratorPlugin = function (outputPath) {
   this.outputPath = outputPath;
@@ -17,19 +19,29 @@ ManifestGeneratorPlugin.prototype.apply = function (compiler) {
 
   compiler.plugin('done', function (stats) {
     var data = stats.toJson(options);
-    var outputData = {};
     var assets = data.assetsByChunkName;
+    var outputData = {};
 
-    _.forOwn(assets, function(value, key) {
-      console.log(value);
-      if (typeof value !== 'string') {
-        // if value is not a string, assume its an array
-        value = value[0];
-      }
-      var ext = value.match(/(\.\w*)$/)[1];
+    function set(item, key) {
+      var ext = path.extname(item);
       var newKey = key + ext;
+      if (outputData[newKey]) {
+        var existsItem = outputData[newKey];
+        delete outputData[newKey];
+        set(existsItem, key + path.extname(path.basename(existsItem, ext)))
+        set(item, key + path.extname(path.basename(item, ext)));
+      } else {
+        outputData[newKey] = item;
+      }
+    }
 
-      outputData[newKey] = value;
+    // { index: ['index.bundle.js', 'index.bundle.css', 'index.bundle.js.map', 'index.bundle.css.map'] }
+    forOwn(assets, function(value, key) {
+      if (Array.isArray(value)) {
+        value.forEach(function(item) { set(item, key) });
+      } else {
+        set(value, key);
+      }
     });
 
     fs.writeFileSync(outputPath, JSON.stringify(outputData));
